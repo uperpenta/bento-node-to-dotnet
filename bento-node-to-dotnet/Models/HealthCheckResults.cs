627a8a0044298daaab0824a47401ea72f3de7e8a
i.raresandrei <i.raresandrei@yahoo.com>
2026-03-29 20:22:46 +0300

    Created models for healtcheck service and response

---
+++ bento-node-to-dotnet/Models/HealthCheckResults.cs
@@ -0,0 +1,12 @@
+﻿namespace bento_node_to_dotnet.Models;
+
+public class HealthCheckResults
+{
+    public Guid Id { get; set; }
+    public Guid MonitoredServiceId { get; set; }
+    public MonitoredService MonitoredService { get; set; } = null!;
+    public int StatusCode { get; set; }
+    public long ResponseTimeMs { get; set; }
+    public bool IsHealthy { get; set; }
+    public DateTime CheckedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ bento-node-to-dotnet/Models/MonitoredService.cs
@@ -0,0 +1,11 @@
+﻿namespace bento_node_to_dotnet.Models;
+
+public class MonitoredService
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public string Status { get; set; } = "Unknown";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? LastCheckedAt { get; set; }
+}
